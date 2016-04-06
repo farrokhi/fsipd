4bd6c0d0d526a9cb304abf63ec551903a137eb26
Babak Farrokhi <babak@farrokhi.net>
2016-04-06 12:42:13 +0430

    update copyright message

--- logfile.c
+++ logfile.c
@@ -1,6 +1,6 @@
 
 /*-
- * Copyright (c) 2015, Babak Farrokhi
+ * Copyright (c) 2016, Babak Farrokhi
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
